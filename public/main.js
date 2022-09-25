f7579283d712692545a69221440e66d3a5f045a0
Mael <contact@maeldev.fr>
2022-09-25 19:50:24 +0200

    Second Version 0.2.0

--- public/main.js
+++ public/main.js
@@ -1,13 +1,18 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require("electron");
+const { app, BrowserWindow } = require("electron");
 require("electron-remote");
 const path = require("path");
 const isDev = require("electron-is-dev");
-require("update-electron-app")();
+const { autoUpdater } = require("electron-updater");
+const log = require("electron-log");
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 900,
+    height: 700,
     icon: "./logo.png",
     webPreferences: {
       enableRemoteModule: true,
@@ -20,8 +25,38 @@ function createWindow() {
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
 }
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message =
+    log_message +
+    " (" +
+    progressObj.transferred +
+    "/" +
+    progressObj.total +
+    ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
+});
 
 app.on("ready", createWindow);
+app.on("ready", function () {
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
 app.on("activate", function () {
   if (process.platform !== "darwin") {
